899e4fe899233fa5b3c57af91c663194fb75037d
siva1024 <siva.radium@gmail.com>
2021-02-02 01:17:54 -0800

    testing multi line comments

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
-RUN apk update && apk add git openssh
+RUN apk update && apk add git openssh sshpass
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,28 +9,22 @@ if [[ -z "$INPUT_HOST" || -z "$INPUT_USERNAME" ]] ; then
 fi
 echo ""
 
-echo $INPUT_HOST
-
-echo $INPUT_USERNAME
 
 echo "Setting up the SSH folders"
 mkdir ~/.ssh/ && chmod 0700 ~/.ssh/
 ssh-keyscan github.com > ~/.ssh/known_hosts
 echo ""
 
-pwd
-
-ls -ltr
-
-ls -ltr /
-
-cat /entrypoint.sh
 
 echo "Setting up the public, private keys and Executables"
 echo $INPUT_KEY > ~/.ssh/id_rsa && echo $INPUT_PUB > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa.pub
 touch /scp-deployer.sh && chmod 700 /scp-deployer.sh
 echo ""
+
+
+echo "Starting the SCP between local and remote"
+echo $INPUT_SOURCE
 
 
 echo "+++++++++++++++++++END PIPELINES+++++++++++++++++++"
