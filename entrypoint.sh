090d56d05ab2ffba9f5be9088397c0ab337f907a
siva1024 <siva.radium@gmail.com>
2021-02-01 23:54:14 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,12 @@ mkdir ~/.ssh/ && chmod 0700 ~/.ssh/
 ssh-keyscan github.com > ~/.ssh/known_hosts
 echo ""
 
+pwd
+
+ls -ltr
+
 echo "Setting up the public, private keys and Executables"
-echo $INPUT_INPUT_KEY > ~/.ssh/id_rsa && echo $INPUT_PUB > ~/.ssh/id_rsa.pub
+echo $INPUT_KEY > ~/.ssh/id_rsa && echo $INPUT_PUB > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa.pub
 touch /scp-deployer.sh && chmod 700 /scp-deployer.sh
 echo ""
