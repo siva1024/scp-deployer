0ccebb97026f24e23bfd5aa8b61f64c0da64030e
siva1024 <siva.radium@gmail.com>
2021-02-01 23:47:50 -0800

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,16 @@
 echo "+++++++++++++++++++STARTING PIPELINES+++++++++++++++++++"
 
 echo "Checking the configurations"
-if [[ -z "$INPUT_HOST" || -z "$INPUT_USERNAME" ]] ; then
+if [[ -z "$INPUT_HOST" || -z "$INPUT_USER" ]] ; then
     echo "No Host found to connect"
     echo "Exiting the Application"
-    echo "Trying uppercase"
-    echo $INPUT_HOST
-    echo "printing User"
-    echo $INPUT_USERNAME
+    exit 1
 fi
 echo ""
+
+echo $INPUT_HOST
+
+echo $INPUT_USER
 
 echo "Setting up the SSH folders"
 mkdir ~/.ssh/ && chmod 0700 ~/.ssh/
@@ -18,7 +19,7 @@ ssh-keyscan github.com > ~/.ssh/known_hosts
 echo ""
 
 echo "Setting up the public, private keys and Executables"
-echo $INPUT_KEY > ~/.ssh/id_rsa && echo $INPUT_PUB > ~/.ssh/id_rsa.pub
+echo $INPUT_INPUT_KEY > ~/.ssh/id_rsa && echo $INPUT_PUB > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa.pub
 touch /scp-deployer.sh && chmod 700 /scp-deployer.sh
 echo ""
